49c257f8503b931e7093559aabc3919c6453a602
phothinh <phothinh01@gmail.com>
2024-02-02 09:10:28 +0100

    Initial Commit

---
+++ UnsplashApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UnsplashApp
+//
+//  Created by Henri Phothinantha on 02/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UnsplashApp/UnsplashAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnsplashAppApp.swift
+//  UnsplashApp
+//
+//  Created by Henri Phothinantha on 02/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct UnsplashAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
